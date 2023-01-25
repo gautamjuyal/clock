ed8749c69bbb280c70ed6331c4518def2e0b3c27
gautamjuyal <gautam.juyal.94@gmail.com>
2023-01-25 21:32:28 +0530

    first build

---
+++ index.js
@@ -0,0 +1,40 @@
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+
+const day = document.querySelector(".day");
+const month = document.querySelector(".month");
+
+setInterval(() => {
+  let time = new Date();
+  let hr = time.getHours() % 12;
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+
+  hour.style.transform = `rotateZ(${(hr * 360) / 12}deg)`;
+  minute.style.transform = `rotateZ(${(min * 360) / 60}deg)`;
+  second.style.transform = `rotateZ(${(sec * 360) / 60}deg)`;
+});
+
+let date = new Date();
+let dy = date.getDate();
+let mnth = date.getMonth();
+
+const months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "oct",
+  "nov",
+  "dec",
+];
+
+day.textContent = dy;
+month.textContent = months[mnth];
